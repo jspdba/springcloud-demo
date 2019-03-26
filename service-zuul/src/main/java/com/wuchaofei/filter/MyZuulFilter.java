2ce308ca72e0490023f078151515d45aa626a701
wuchaofei <wuchaofei@cofco.com>
2019-03-26 18:47:27 +0800

    zuul过滤器

---
+++ service-zuul/src/main/java/com/wuchaofei/filter/MyZuulFilter.java
@@ -0,0 +1,75 @@
+package com.wuchaofei.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author wuchaofei
+ * Created by wuchaofei on 2019/3/26.
+ */
+@Component
+public class MyZuulFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(MyZuulFilter.class);
+
+    /**
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤的顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    /**
+     * 过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
