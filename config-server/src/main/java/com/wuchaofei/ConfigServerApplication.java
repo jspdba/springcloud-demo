d66c086f6502e94f3de41bb18c5cb2e9648f9370
wuchaofei <wuchaofei@cofco.com>
2019-03-27 18:32:00 +0800

    添加 config-server, config-client配置中心

--- config-server/src/main/java/com/wuchaofei/ConfigServerApplication.java
+++ config-server/src/main/java/com/wuchaofei/ConfigServerApplication.java
@@ -3,9 +3,12 @@ package com.wuchaofei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class ConfigServerApplication {
 
