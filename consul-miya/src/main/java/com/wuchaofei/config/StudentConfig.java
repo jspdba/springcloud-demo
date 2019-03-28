d10af2c4506e64bc5cc5c0f38adf3bf983d5973b
wuchaofei <wuchaofei@cofco.com>
2019-03-28 18:43:55 +0800

    添加说明

--- consul-miya/src/main/java/com/wuchaofei/config/StudentConfig.java
+++ consul-miya/src/main/java/com/wuchaofei/config/StudentConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  *
  * @author wuchaofei
+ * 在本文中consul作为配置中心，有一个点需要注意，通过@Value注入的属性，修改consul的配置后，属性不能立即生效，需要服务重启。而通过@ConfigurationProperties注入的属性，修改consul的配置后，属性会立即生效，所以建议如果需要动态生效的配置，最好使使用@ConfigurationProperties进行属性的注入。
  * Created by cofco on 2019/3/28.
  */
 @ConfigurationProperties(prefix = "student")
